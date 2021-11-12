f3cf27ecf3dc311540a825a2f086c78de2fe1688
S0532 <S0532@20S0532>
2021-11-12 12:14:18 +0900

    初コミットです

---
+++ 2A_watanabe_kouki/src/main/java/com/example/demo/controller/JpaPracticeController.java
@@ -0,0 +1,19 @@
+//package com.example.demo.controller;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import com.example.demo.repository.ShopRepository;
+//@Controller
+//public class JpaPracticeController {
+//    
+//    @RequestMapping(path = "/jpaPractice", method = RequestMethod.GET)
+//    public String viewPage(Model model) {
+//        
+//        
+//        // 表示するHTMLはreturnの後ろに書く。
+//        // 今回の場合はtemplatesフォルダの中にあるsessionフォルダにあるlogin.htmlを表示する。
+//        return "jpaPractice";
+//    }
+//}
---
+++ 2A_watanabe_kouki/src/main/java/com/example/demo/entity/ItemPractice.java
@@ -0,0 +1,35 @@
+//package com.example.demo.entity;
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//@Entity
+//@Table(name = "itemPractice")
+//public class ItemPractice {
+//    @Id
+//    private Integer no;
+//    
+//    @Column
+//    private String name;
+//    
+//    @Column
+//    private Integer price;
+//    public Integer getId() {
+//        return no;
+//    }
+//    public void setId(Integer no) {
+//        this.no = no;
+//    }
+//    public String getName() {
+//        return name;
+//    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//    public Integer getPrice() {
+//        return price;
+//    }
+//    public void setPrice(Integer price) {
+//        this.price = price;
+//    }
+//}
